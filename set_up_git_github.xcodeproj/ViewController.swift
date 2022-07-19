f592b1bdb8ad569d53752bf8f8c4b86451bc8d51
Lauren <lgikick@gmail.com>
2022-07-19 13:47:15 -0400

    Initial Commit

---
+++ set_up_git_github.xcodeproj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  set_up_git_github.xcodeproj
+//
+//  Created by Lauren Italia on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
